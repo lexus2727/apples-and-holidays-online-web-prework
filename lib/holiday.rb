7f64575ca45f88dc225899e38e8a61f7f1d35217
Candice Parker <lexus2424@gmail.com>
2019-05-02 15:23:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,7 +88,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, data|
     data.each do |attribute, value|
       value.each do |supply|
-        if attribute == "BBQ"
+        if supply == "BBQ"
           puts "#{supply}"
           answer.push(holiday)
           
